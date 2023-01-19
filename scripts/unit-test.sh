97894c233a8329e4bbee4c454a4eed04fc4fb900
Nishant Vaity <enishant@users.noreply.github.com>
2023-01-19 19:52:00 +0530

    Update unit-test.sh

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -18,10 +18,12 @@ set -e
 if [ $COMPOSER_COMPATIBILITY = 0 ]
 then
 	/usr/local/bin/composer install --prefer-dist --no-progress
+	/usr/local/bin/composer require razorpay/magento:dev-ut-test-3
 else
   echo "using composer v1"
   php7.2 /usr/local/bin/composer self-update --1
 	/usr/local/bin/composer install --prefer-dist --no-progress
+	/usr/local/bin/composer require razorpay/magento:dev-ut-test-3
 fi
 
 chmod +x bin/magento
@@ -52,8 +54,6 @@ else
     bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation --disable-modules="$INPUT_DISABLE_MODULES"
   fi
 fi
-
-/usr/local/bin/composer require razorpay/magento:dev-ut-test-3
 
 bin/magento module:enable Razorpay_Magento
 
